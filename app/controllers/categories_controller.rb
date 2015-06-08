3ce514f14c9593793467bd28d7e2f6c4fa6a629f
neudabei <rschultzejena@gmail.com>
2015-06-08 22:46:42 +0100

    Added forms for categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,42 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category has been created."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+end
